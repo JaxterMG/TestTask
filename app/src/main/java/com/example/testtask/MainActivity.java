f913455074491a7fe4a52d8c1577652cada91f52
Jaxter <eugeny.craevsky@yandex.ru>
2021-06-29 17:02:06 +0300

    Скачивание файла и обработка
    Перевел скачивание и считывание в отдельные потоки

--- app/src/main/java/com/example/testtask/MainActivity.java
+++ app/src/main/java/com/example/testtask/MainActivity.java
@@ -3,102 +3,142 @@ package com.example.testtask;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.JsonReader;
 import android.webkit.URLUtil;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+
+    String title;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-        DownloadJson();
+        DownloadFile downloadFile = new DownloadFile();
+        downloadFile.execute();
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.text_view_id);
     }
 
-   
 
-    void DownloadJson()
-    {
-        String url = getString(R.string.URL);
-        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-        String title = URLUtil.guessFileName(url,null,null);
-        request.setTitle(title);
-        request.setDescription("Downloading file");
-        request.addRequestHeader("cookie", url);
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_HIDDEN);
-        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, title);
 
-        DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-        downloadManager.enqueue(request);
-
-        Toast.makeText(MainActivity.this, "DownloadingStarted", Toast.LENGTH_SHORT).show();
-
-        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),title);
-        if(file.exists())
-        {
-            Toast.makeText(MainActivity.this, "Exists", Toast.LENGTH_SHORT).show();
-            ParseFile(file);
+    BroadcastReceiver onComplete=new BroadcastReceiver() {
+        public void onReceive(Context ctxt, Intent intent) {
+            Toast.makeText(MainActivity.this, "Downloaded", Toast.LENGTH_SHORT).show();
+          ParseTextFile parse = new ParseTextFile();
+          parse.execute(title);
         }
-        else
-        {
-            Toast.makeText(MainActivity.this, "No such file", Toast.LENGTH_SHORT).show();
+    };
+
+
+    private  class DownloadFile extends AsyncTask<Void,Void,Void>
+    {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            String url = getString(R.string.URL);
+            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+            title = URLUtil.guessFileName(url,null,null);
+            request.setTitle(title);
+            request.setDescription("Downloading file");
+            request.addRequestHeader("cookie", url);
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_HIDDEN);
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, title);
+
+            DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+            downloadManager.enqueue(request);
+            //Toast.makeText(MainActivity.this, "DownloadingStarted", Toast.LENGTH_SHORT).show();
+            registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+
+            return null;
         }
     }
-    void ParseFile(File fileName)
+
+
+    private class ParseTextFile extends  AsyncTask<String, Void, String>
     {
-        StringBuilder text = new StringBuilder();
 
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(fileName));
-            String line;
+        @Override
+        protected String doInBackground(String... strings) {
+           /* int i = 0;
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),strings[0]);
+            try {
+                InputStream is = new FileInputStream(file);
+                Scanner sc = new Scanner(is);
+                //Reading line by line from scanner to StringBuffer
+                StringBuffer sb = new StringBuffer();
+                while(sc.hasNext()){
+                    i++;
+                    sb.append(sc.nextLine());
+                    System.out.println(i+" " +sc.nextLine());
+                }
 
-            while ((line = br.readLine()) != null) {
-                text.append(line);
-                text.append('\n');
+                // ConvertToJson(sb.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+            file.delete();
+            return null;
 
-            br.close();
+            */
+            String jsontext = null;
+            byte[] buffer = null;
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),strings[0]);
+            try {
+                InputStream is = new FileInputStream(file);
+                buffer = new byte[is.available()];
+            while (is.read(buffer) != -1);
 
-            ConvertToJson(text);
-        }
-        catch (IOException e) {
 
-        }
-        fileName.delete();
-
-    }
-    void ConvertToJson(StringBuilder stringBuilder)
-    {
-        try
-        {
-            JSONArray mainObject = new JSONArray(stringBuilder.toString());
-
-            for (int i = 0; i < mainObject.length(); i++)
-            {
-                JSONObject object = mainObject.getJSONObject(i);
-                int id = object.getInt("id");
-
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }
-        catch (JSONException e)
-        {
-            e.printStackTrace();
-        }
+            jsontext = new String(buffer);
+            //System.out.println(jsontext);
+            file.delete();
+
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = new JSONObject(jsontext);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            try {
+                JSONArray cities = jsonObject.getJSONArray("city");
+                System.out.println(cities.get(1));
+                //tv.setText(name);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
 
 
+
+
+        }
     }
 }
+
+
