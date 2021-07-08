6e16934e8217eb33360d3d8a1099718e8cdcdaf3
Jaxter <eugeny.craevsky@yandex.ru>
2021-07-08 19:10:11 +0300

    Making cells manually

--- app/src/main/java/com/example/testtask/TerminalFragments/FromFragment.java
+++ app/src/main/java/com/example/testtask/TerminalFragments/FromFragment.java
@@ -1,5 +1,6 @@
 package com.example.testtask.TerminalFragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,20 +8,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.example.testtask.MainActivity;
 import com.example.testtask.R;
+import com.example.testtask.TerminalsScreen;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link FromFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FromFragment extends Fragment {
+public class FromFragment extends Fragment implements  View.OnClickListener{
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    LinearLayout layoutList;
+    Button add;
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -60,7 +70,34 @@ public class FromFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_from, container, false);
+        View view = inflater.inflate(R.layout.fragment_from, container, false);
+        layoutList = view.findViewById(R.id.layout_list);
+        add = view.findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                AddView();
+            }
+        });
+        return view;
+    }
+
+
+    private void AddView()
+    {
+        View cellView = getLayoutInflater().inflate(R.layout.terminal_cell_layout, null, false);
+        System.out.println("Clicked");
+        layoutList.addView(cellView);
+    }
+    private  void  DeleteView(View v)
+    {
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
--- app/src/main/java/com/example/testtask/TerminalFragments/ToFragment.java
+++ app/src/main/java/com/example/testtask/TerminalFragments/ToFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.testtask.R;
 
@@ -25,6 +26,8 @@ public class ToFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    TextView tv;
 
     public ToFragment() {
         // Required empty public constructor
@@ -61,6 +64,7 @@ public class ToFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        
         return inflater.inflate(R.layout.fragment_to, container, false);
     }
 }
